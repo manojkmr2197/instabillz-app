9cb51789e23fc4561b105d60b69f0a9bcbe92430
Mano <manojkmr2197@gmail.com>
2025-09-24 08:04:53 +0530

    Initial commit

---
+++ app/src/main/java/com/app/billing/instabillz/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.app.billing.instabillz.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.app.billing.instabillz.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
