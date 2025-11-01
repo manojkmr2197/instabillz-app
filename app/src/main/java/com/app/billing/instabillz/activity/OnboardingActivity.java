2b74afe4b6ba3e3ca79a0f2c908106e8f9986d0f
Mano <manojkmr2197@gmail.com>
2025-11-01 13:12:28 +0530

    product list page design enhanced

---
+++ app/src/main/java/com/app/billing/instabillz/activity/OnboardingActivity.java
@@ -0,0 +1,17 @@
+package com.app.billing.instabillz.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.app.billing.instabillz.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+    }
+}
--- app/src/main/java/com/app/billing/instabillz/viewholder/ProductViewHolder.java
+++ app/src/main/java/com/app/billing/instabillz/viewholder/ProductViewHolder.java
@@ -2,6 +2,7 @@ package com.app.billing.instabillz.viewholder;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,13 +12,13 @@ import com.app.billing.instabillz.R;
 
 public class ProductViewHolder extends RecyclerView.ViewHolder {
 
-    public Button delete, edit;
+    public ImageButton delete, edit;
     public TextView productName,productPrice;
     public ProductViewHolder(@NonNull View itemView) {
         super(itemView);
         productName = (TextView) itemView.findViewById(R.id.product_item_name);
         productPrice = (TextView) itemView.findViewById(R.id.product_item_price);
-        delete = (Button) itemView.findViewById(R.id.product_item_close);
-        edit = (Button) itemView.findViewById(R.id.product_item_edit);
+        delete = (ImageButton) itemView.findViewById(R.id.product_item_close);
+        edit = (ImageButton) itemView.findViewById(R.id.product_item_edit);
     }
 }
